33175b8b8169784ddc4d333261ac36197d4006cb
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-07-20 14:15:51 +0100

    Add launch command

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ app = typer.Typer()
 def main(name: str):
     print(f"Hello {name}")
 
+@app.command()
+def launch():
+    typer.echo("Launching the application...")
+    typer.launch("https://github.com/JackPlowman/dev-cli-typer-experiment")
 
 if __name__ == "__main__":
     app()
